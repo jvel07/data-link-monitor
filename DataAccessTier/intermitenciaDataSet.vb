b49aa2f24d42482f5639f7821b33ab9f2831883f
José Vicente Egas-López <joseegaslopez@gmail.com>
2021-01-26 00:25:00 +0100

    Add files via upload
    added data access tier

---
+++ DataAccessTier/intermitenciaDataSet.vb
@@ -0,0 +1,9 @@
+﻿Namespace intermitenciaDataSetTableAdapters
+    
+    Partial Class Info_FarmaciaTableAdapter
+
+    End Class
+
+    Partial Public Class Estado_EnlaceTableAdapter
+    End Class
+End Namespace
