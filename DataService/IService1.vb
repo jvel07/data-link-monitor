f5fa696c73a8962fe735c85bdad213df44efe6d7
José Vicente Egas-López <joseegaslopez@gmail.com>
2021-01-26 00:27:26 +0100

    Add files via upload

---
+++ DataService/IService1.vb
@@ -0,0 +1,55 @@
+﻿' NOTE: You can use the "Rename" command on the context menu to change the interface name "IService1" in both code and config file together.
+<ServiceContract()>
+Public Interface IService1
+
+    <OperationContract()>
+    Function GetData(ByVal value As Integer) As String
+
+    <OperationContract()>
+    Function GetDataUsingDataContract(ByVal composite As CompositeType) As CompositeType
+
+    ' TODO: Add your service operations here
+
+
+    'FUNCIONES ESTADO ENLACE
+
+    <OperationContract()>
+    Function insertarEstadoEnlace(ByVal id_estado_enlace As Integer, ByVal porcent_pings_perd As Double, ByVal tiempo_monitoreo As Integer, ByVal fk_id_farm As Integer, ByVal ip_farm As String, ByVal nombre_farm As String) As Integer
+
+    <OperationContract()>
+    Function obtenerEstados() As DataEntityTier.intermitenciaDataSet.Estado_EnlaceDataTable
+
+    <OperationContract()>
+    Function modificarEstadoEnlace(ByVal id_estado_enlace As Integer, ByVal porcent_pings_perd As Double, ByVal tiempo_monitoreo As Integer, ByVal fk_id_farm As Integer, ByVal ip_farm As String, ByVal nombre_farm As String) As Integer
+
+    <OperationContract()>
+    Function eliminarEstadoEnlace() As Integer
+
+    'FUNCIONES INFO FARMACIA
+
+    <OperationContract()>
+    Function insertarFarmacia(ByVal id_farmacia As Integer, ByVal nombre_farmacia As String, ByVal ip_farmacia As String, ByVal rec_alerta As String) As Integer
+
+    <OperationContract()>
+    Function obtenerFarmacias() As DataEntityTier.intermitenciaDataSet.Info_FarmaciaDataTable
+
+    <OperationContract()> _
+    Function modificarFarmacia(ByVal codigoFarmacia As Integer, ByVal nombreFarmacia As String, ByVal ipFarmacia As String, ByVal rec_alerta As String) As Integer
+
+
+
+
+End Interface
+
+' Use a data contract as illustrated in the sample below to add composite types to service operations
+
+<DataContract()>
+Public Class CompositeType
+
+    <DataMember()>
+    Public Property BoolValue() As Boolean
+
+    <DataMember()>
+    Public Property StringValue() As String
+
+End Class
---
+++ DataService/Service1.vb
@@ -0,0 +1,64 @@
+﻿' NOTE: You can use the "Rename" command on the context menu to change the class name "Service1" in both code and config file together.
+Public Class Service1
+    Implements IService1
+
+    Public Function GetData(ByVal value As Integer) As String Implements IService1.GetData
+        Return String.Format("You entered: {0}", value)
+    End Function
+
+    Public Function GetDataUsingDataContract(ByVal composite As CompositeType) As CompositeType Implements IService1.GetDataUsingDataContract
+        If composite Is Nothing Then
+            Throw New ArgumentNullException("composite")
+        End If
+        If composite.BoolValue Then
+            composite.StringValue &= "Suffix"
+        End If
+        Return composite
+    End Function
+
+    'ESTADOS
+    Public Function insertarEstadoEnlace(ByVal id_estado_enlace As Integer, ByVal porcent_pings_perd As Double, ByVal tiempo_monitoreo As Integer, ByVal fk_id_farm As Integer, ByVal ip_farm As String, ByVal nombre_farm As String) As Integer Implements IService1.insertarEstadoEnlace
+        Dim Estado_EnlaceTableAdapter As New DataAccessTier.intermitenciaDataSetTableAdapters.Estado_EnlaceTableAdapter()
+        Return Estado_EnlaceTableAdapter.Insert(id_estado_enlace, porcent_pings_perd, tiempo_monitoreo, fk_id_farm, ip_farm, nombre_farm)
+
+    End Function
+
+    Public Function obtenerEstados() As DataEntityTier.intermitenciaDataSet.Estado_EnlaceDataTable Implements IService1.obtenerEstados
+        Dim Estado_EnlaceTableAdapter As New DataAccessTier.intermitenciaDataSetTableAdapters.Estado_EnlaceTableAdapter()
+        Return Estado_EnlaceTableAdapter.GetEstados
+
+    End Function
+
+    Public Function modificarEstadoEnlace(ByVal id_estado_enlace As Integer, ByVal porcent_pings_perd As Double, ByVal tiempo_monitoreo As Integer, ByVal fk_id_farm As Integer, ByVal ip_farm As String, ByVal nombre_farm As String) As Integer Implements IService1.modificarEstadoEnlace
+        Dim Estado_EnlaceTableAdapter As New DataAccessTier.intermitenciaDataSetTableAdapters.Estado_EnlaceTableAdapter()
+        Return Estado_EnlaceTableAdapter.UpdateEstado(porcent_pings_perd, tiempo_monitoreo, fk_id_farm, ip_farm, nombre_farm, id_estado_enlace)
+
+    End Function
+
+    Public Function eliminarEstadoEnlace() As Integer Implements IService1.eliminarEstadoEnlace
+        Dim Estado_EnlaceTableAdapter As New DataAccessTier.intermitenciaDataSetTableAdapters.Estado_EnlaceTableAdapter()
+        Return Estado_EnlaceTableAdapter.DeleteEstados
+    End Function
+    'FARMACIAS
+    Public Function insertarFarmacia(ByVal id_farmacia As Integer, ByVal nombre_farmacia As String, ByVal ip_farmacia As String, ByVal rec_alerta As String) As Integer Implements IService1.insertarFarmacia
+        Dim Info_FarmaciaTableAdapter As New DataAccessTier.intermitenciaDataSetTableAdapters.Info_FarmaciaTableAdapter()
+        Return Info_FarmaciaTableAdapter.InsertFarmacia(id_farmacia, nombre_farmacia, ip_farmacia, rec_alerta)
+    End Function
+
+    Public Function modificarFarmacia(ByVal idFarmacia As Integer, ByVal nombreFarmacia As String, ByVal ipFarmacia As String, ByVal rec_alerta As String) As Integer Implements IService1.modificarFarmacia
+        Dim Info_FarmaciaTableAdapter As New DataAccessTier.intermitenciaDataSetTableAdapters.Info_FarmaciaTableAdapter()
+        Return Info_FarmaciaTableAdapter.UpdateFarmacia(nombreFarmacia, ipFarmacia, rec_alerta, idFarmacia)
+
+    End Function
+    Public Function obtenerFarmacias() As DataEntityTier.intermitenciaDataSet.Info_FarmaciaDataTable Implements IService1.obtenerFarmacias
+        Dim Info_FarmaciaTableAdapter As New DataAccessTier.intermitenciaDataSetTableAdapters.Info_FarmaciaTableAdapter()
+        Return Info_FarmaciaTableAdapter.GetFarmacias
+    End Function
+
+
+
+
+
+
+   
+End Class
